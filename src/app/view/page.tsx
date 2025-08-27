3f96601ec14f4b2e2cd2d10a4fe3a5d657ced0e5
khalid ibrahim <khalidaltegani3@gmail.com>
2025-08-27 10:40:15 +0000

    ممتاز لقد عاد للعمل الان يجب ان نقوم بمعايرة المقاسات في صفحة الفيو بحيث

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -359,7 +359,7 @@ export default function ViewPage() {
     };
 
     return (
-        <div className="h-full w-full bg-black snap-y snap-mandatory overflow-y-auto">
+        <div className="h-full w-full bg-black snap-y snap-mandatory">
            {videos.map(video => (
                <VideoCard 
                     key={video.id} 

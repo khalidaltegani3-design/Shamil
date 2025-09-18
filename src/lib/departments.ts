d3c5e7c4628571f4e212bd2aebec5d3e004a3212
khalid ibrahim <khalidaltegani3@gmail.com>
2025-09-18 10:39:57 +0000

    يجب ان تحذف اسماء الادارات التي قمت انت بانشائها وتستبدلها ولتكن الادارا

--- src/lib/departments.ts
+++ src/lib/departments.ts
@@ -1,10 +1,8 @@
 
 export const allDepartments = [
-  { id: "executive-management", name: "الإدارة العليا" },
-  { id: "it-support", name: "الدعم الفني" },
-  { id: "maintenance", name: "الصيانة" },
-  { id: "general-services", name: "الخدمات العامة" },
-  { id: "human-resources", name: "الموارد البشرية" },
-  { id: "public-works", name: "الأشغال العامة"},
-  { id: "municipal-inspections", name: "التفتيش البلدي"},
+  { id: "general-monitoring", name: "الرقابة العامة" },
+  { id: "health-monitoring", name: "الرقابة الصحية" },
+  { id: "technical-monitoring", name: "الرقابة الفنية" },
+  { id: "services-affairs", name: "شؤون الخدمات" },
+  { id: "other-violations", name: "مخالفات اخرى" },
 ];
